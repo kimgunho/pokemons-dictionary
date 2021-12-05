ffa4dcdd8653c1ee306c523a9b0a69dcb3e98115
kim <yuyukgh@naver.com>
2021-12-06 04:39:52 +0900

    feat : eslintrc, prettier 재생성

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,69 +1,69 @@
-import React, { useEffect, useState, useRef } from 'react'
-import Search from './search'
-import Pokemons from './pokemons'
-import Skeleton from './Skeleton'
+import React, { useEffect, useState, useRef } from "react";
+import Search from "./search";
+import Pokemons from "./pokemons";
+import Skeleton from "./Skeleton";
 
-import { UseUserPokemons } from '../../context/pokemonContext'
+import { UseUserPokemons } from "../../context/pokemonContext";
 
 function List() {
-  const { pokemons, setPokemons, count, setCount } = UseUserPokemons()
-  const [isLoading, setIsLoading] = useState(true)
+  const { pokemons, setPokemons, count, setCount } = UseUserPokemons();
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchPokemons = async (count) => {
     const res = await fetch(
       //
-      `https://pokeapi.co/api/v2/pokemon?limit=${count}`,
-    )
-    const json = await res.json()
-    const { results } = json
+      `https://pokeapi.co/api/v2/pokemon?limit=${count}`
+    );
+    const json = await res.json();
+    const { results } = json;
 
     const pokemonItem = await Promise.all(
       results.map(async ({ url }) => {
-        const pokemonRes = await fetch(url)
-        const pokemonJson = await pokemonRes.json()
-        const detailUrl = pokemonJson.species.url
+        const pokemonRes = await fetch(url);
+        const pokemonJson = await pokemonRes.json();
+        const detailUrl = pokemonJson.species.url;
 
-        const detailRes = await fetch(detailUrl)
-        const detailJson = await detailRes.json()
+        const detailRes = await fetch(detailUrl);
+        const detailJson = await detailRes.json();
 
         return {
           id: pokemonJson.id,
           name: detailJson.names[2].name,
-          img: pokemonJson.sprites.other['official-artwork'].front_default,
+          img: pokemonJson.sprites.other["official-artwork"].front_default,
           type: pokemonJson.types[0].type.name,
           color: detailJson.color.name,
           text: detailJson.flavor_text_entries[23].flavor_text,
           genera: detailJson.genera[1].genus,
           height: pokemonJson.height,
           weight: pokemonJson.weight,
-        }
-      }),
-    )
+        };
+      })
+    );
 
-    setPokemons(pokemonItem)
-    setIsLoading(false)
-  }
+    setPokemons(pokemonItem);
+    setIsLoading(false);
+  };
 
   const loadMore = () => {
-    setCount((acc) => acc + 9)
-  }
+    setCount((acc) => acc + 9);
+  };
 
-  useEffect(() => fetchPokemons(count), [count])
+  useEffect(() => fetchPokemons(count), [count]);
 
-  const conutEnd = useRef()
+  const conutEnd = useRef();
   useEffect(() => {
     if (!isLoading) {
       const observer = new IntersectionObserver(
         (entrise) => {
           if (entrise[0].isIntersecting) {
-            loadMore()
+            loadMore();
           }
         },
-        { threshold: 0.2 },
-      )
-      observer.observe(conutEnd.current)
+        { threshold: 0.2 }
+      );
+      observer.observe(conutEnd.current);
     }
-  }, [isLoading])
+  }, [isLoading]);
 
   return (
     <div>
@@ -74,7 +74,7 @@ function List() {
         <Pokemons more={loadMore} end={conutEnd} item={pokemons} />
       )}
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
